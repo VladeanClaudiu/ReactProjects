45fcd832f81c3abd1c3027ce41f595f347e58418
Claudiu Vladean <claudiuver3.2@gmail.com>
2022-07-28 15:32:06 +0100

    messed around with some jsx

--- ReactInfo/index.js
+++ ReactInfo/index.js
@@ -1,15 +1,15 @@
-function UnorderedList() {
-  return (
-    <ul>
-      <li>Thing 1</li>
-      <li>Thing 2</li>
-    </ul>
-  );
-}
-
-ReactDOM.render(
+const page = (
   <div>
-    <UnorderedList />
-  </div>,
-  document.getElementById("root")
+    <img src="./images/react-logo.png" width="40px"></img>
+    <h1>Fun facts about React</h1>
+    <ul>
+      <li>Was first released in 2013</li>
+      <li>Was originally created by Jordan Walke</li>
+      <li>Has well over 100k stars on Github</li>
+      <li>Is maintained by Facebook</li>
+      <li>Powers thousands of enterpriese apps, including mobile apps</li>
+    </ul>
+  </div>
 );
+
+ReactDOM.render(page, document.getElementById("root"));
