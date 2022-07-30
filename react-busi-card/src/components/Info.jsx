4b33313ac0ef60278b3f2acbb0a2f67ab04139db
Claudiu Vladean <claudiuver3.2@gmail.com>
2022-07-30 14:12:42 +0100

    intro section of the card finsished

--- react-busi-card/src/components/Info.jsx
+++ react-busi-card/src/components/Info.jsx
@@ -3,8 +3,12 @@ export default function Info(){
         <div className="info-section">
             <div className="info-photo"></div>
             <h1>Claudiu Vladean</h1>
-            <a href="#">www.claudiuvladea.com</a>
-            <p>button</p>
+            <h4>Frontend Developer</h4>
+            <a href="#">claudiuvladea.com</a>
+            <div className="info-buttons">
+                <button className="button-email">Email</button>
+                <button className="button-linkedIn">LinkedIn</button>
+            </div>
         </div>
     )
 }
