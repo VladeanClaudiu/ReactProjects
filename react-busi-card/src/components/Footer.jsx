c75cdfaae9f87c09e9ad181f554db7c9cd7f15a9
Claudiu Vladean <claudiuver3.2@gmail.com>
2022-07-31 13:29:43 +0100

    svg buttons added to the footer along with destinations for the majority of the buttons

--- react-busi-card/src/components/Footer.jsx
+++ react-busi-card/src/components/Footer.jsx
@@ -1,9 +1,22 @@
 export default function Footer() {
     return(
-        <footer>
-            <p>icon</p>
-            <p>icon</p>
-            <p>icon</p>
+        <footer className="footer-section">
+            <a href="https://github.com/VladeanClaudiu">
+                <div className="footer-github-icon">
+                </div>
+            </a>
+            <a href="https://www.instagram.com/cvllaauddieuan/">
+                <div className="footer-instagram-icon">
+                </div>
+            </a>
+            <a href="#">
+                <div className="footer-facebook-icon">
+                </div>
+            </a>
+            <a href="#">
+                <div className="footer-twitter-icon">
+                </div>
+            </a>
         </footer>
     )
 }
--- react-busi-card/src/components/Info.jsx
+++ react-busi-card/src/components/Info.jsx
@@ -6,8 +6,12 @@ export default function Info(){
             <h4>Frontend Developer</h4>
             <a href="#">claudiuvladea.com</a>
             <div className="info-buttons">
-                <button className="button-email">Email</button>
-                <button className="button-linkedIn">LinkedIn</button>
+                <button onClick={()=>{
+                    window.open('mailto:claudiuver1.0@gmail.com');
+                }} className="button-email">Email</button>
+                <button onClick={()=>{
+                    location.href = "https://www.linkedin.com/in/claudiu-vladean-652614191/";
+                }} className="button-linkedIn">LinkedIn</button>
             </div>
         </div>
     )
