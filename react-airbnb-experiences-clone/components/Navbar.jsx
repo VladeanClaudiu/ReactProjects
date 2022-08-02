08d7768f7118a173aa85c13c4a13f4da93190496
Claudiu Vladean <claudiuver3.2@gmail.com>
2022-08-02 11:46:16 +0100

    created the nav componet and set up the css files

---
+++ react-airbnb-experiences-clone/components/Navbar.jsx
@@ -0,0 +1,8 @@
+import Logo from "../src/assets/images/airbnb-logo.png"
+export default function Navbar(){
+    return ( 
+        <nav className="nav-section">
+            <img className="nav-logo" src={Logo} alt="" srcset="" />  
+        </nav>
+    )
+}
--- react-airbnb-experiences-clone/src/App.jsx
+++ react-airbnb-experiences-clone/src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import Navbar from '../components/Navbar'
 
 function App() {
 
   return (
     <div className="App">
-     
+     <Navbar />
     </div>
   )
 }
