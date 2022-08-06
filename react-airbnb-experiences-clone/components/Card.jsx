b4bebc2e951adeaeed04e9814c75b3a9f26a4266
Claudiu Vladean <claudiuver3.2@gmail.com>
2022-08-06 13:21:57 +0100

    style for each card

--- react-airbnb-experiences-clone/components/Card.jsx
+++ react-airbnb-experiences-clone/components/Card.jsx
@@ -2,7 +2,7 @@ export default function Card(props){
     const {available, image, stars, rating, location, title, price} = props;
     return (
         <div className="card">
-            {props.hasOwnProperty('available') ? <div className="card-available">{available ? 'ONLINE' : 'SOLD OUT'}</div> : null}    
+            {props.hasOwnProperty('available') ? <div className="card-available">{available ? <p>ONLINE</p> : <p>SOLD OUT</p>}</div> : null}    
             <img src={image} alt="card image" className="card-image" />
             <div className="card-info">
                 <img src="../src/assets/images/star.png" className="card-star" />
