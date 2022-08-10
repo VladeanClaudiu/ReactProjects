31d97afba42dc708dc365fa6cd1af0c4cd08af07
Claudiu Vladean <claudiuver3.2@gmail.com>
2022-08-10 16:06:48 +0100

    nav bar component complete

---
+++ react-meme-generator/components/Nav.jsx
@@ -0,0 +1,8 @@
+export default function Nav() {
+    return (
+        <nav className="nav-section">
+            <h4>Meme Generator</h4>
+            <p>React - Project 3</p>
+        </nav>
+    )
+}
