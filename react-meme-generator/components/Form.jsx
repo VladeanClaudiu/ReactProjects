c1ae509c924e41df746659a2b19d1ac0f7a56e94
Claudiu Vladean <claudiuver3.2@gmail.com>
2022-08-10 16:09:41 +0100

    created from component

---
+++ react-meme-generator/components/Form.jsx
@@ -0,0 +1,11 @@
+export default function Form() {
+    return (
+        <section className="form-section">
+            <div>
+                <input type="text" />
+                <input type="text" />
+            </div>
+            <button>Generate New Meme</button>
+        </section>
+    )
+}
--- react-meme-generator/src/App.jsx
+++ react-meme-generator/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import Nav from '../components/Nav'
+import Form from '../components/Form'
 
 function App() {
 
   return (
     <div>
       <Nav />
+      <Form />
     </div>
   )
 }
