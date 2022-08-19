253f45f79565e5fa80638a1ab8edeb7cefe44296
Claudiu Vladean <claudiuver3.2@gmail.com>
2022-08-19 16:39:48 +0100

    code formatting

--- react-meme-generator/components/Image.jsx
+++ react-meme-generator/components/Image.jsx
@@ -1,6 +1,7 @@
 export default function Image(props) {
     const {img} = props;
     return (
+        
         <section className="image-section">
             <img src={img}/>
         </section>
