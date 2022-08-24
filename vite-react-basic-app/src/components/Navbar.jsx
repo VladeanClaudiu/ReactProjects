d1fe28dd61d95f5e7a55a9f24efbe4c3ddd0f26c
Claudiu Vladean <claudiuver3.2@gmail.com>
2022-08-24 15:03:51 +0100

    style changes for the state toggle

--- vite-react-basic-app/src/components/Navbar.jsx
+++ vite-react-basic-app/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ export default function NavBar(props){
     return(
         <nav className={props.darkMode? "dark" : ""}>
             <img className="react-logo" src={Logo} />
-            <h3>What is React?</h3>
+            <h3 className='nav-logo-text'>What is React?</h3>
             <div className="toggler">
                 <p className="toggler-light">Light</p>
                 <div className="toggler-slider" onClick={props.toggleMode}>
