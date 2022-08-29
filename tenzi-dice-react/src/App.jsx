2b625cd0eaa6e058c283b456c4dedb4a56fd4241
VladeanClaudiu <claudiuver3.2@gmail.com>
2022-08-29 18:57:11 +0100

    confetti styled

--- tenzi-dice-react/src/App.jsx
+++ tenzi-dice-react/src/App.jsx
@@ -70,7 +70,7 @@ function App() {
   
   return (
     <main className='tenzi-main'>
-      {tenzi && <Confetti/>}
+      {tenzi && <Confetti className='confetti-width'/>}
       <h1 className='tenzi-title'>Tenzies</h1>
       <p className="tenzi-instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <section className='die-section'>
