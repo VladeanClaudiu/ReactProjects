2181f26befdd837fbf176202e09c5a0650d41a70
VladeanClaudiu <claudiuver3.2@gmail.com>
2022-08-31 10:41:14 +0100

    working on api fetch

--- react-quiz-app/src/App.jsx
+++ react-quiz-app/src/App.jsx
@@ -2,13 +2,27 @@ import { useState } from 'react'
 import './App.css'
 import StartPage from './components/StartPage'
 import Question from './components/Question'
+import { useEffect } from 'react'
 
 function App() {
   const [gameStart, setGameStart] = useState(false)
+  const [quizState, setQuizState] = useState([])
 
   function changeGameState() {
     setGameStart(oldGame => !oldGame)
   }
+
+  useEffect(()  => {
+    const fetchData = async() => {
+    const data = await fetch('https://opentdb.com/api.php?amount=10&difficulty=medium')
+    const jsonData =  data.json();
+     return await jsonData;
+    } 
+
+    const result = fetchData()
+    .catch(console.error);
+     console.log(result)
+  },[gameStart])
 
   return (
     <main>
@@ -16,7 +30,7 @@ function App() {
       handleClick={changeGameState}
       />}
       {gameStart && 
-      <section className='qeustion-section'><Question />
+      <section className='question-section'><Question />
       </section>}
       {/* ^^^ main quiz component to be added here :) */}
     </main>
--- react-quiz-app/src/components/Question.jsx
+++ react-quiz-app/src/components/Question.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 
-export default function Question() {
+export default function Question(props) {
     return(
         <section className="question">
              <h3 className="question-head">Question</h3>
             <div className="question-options">
-                <div><h4>Option 1</h4></div>
-                <div><h4>Option 2</h4></div>
-                <div><h4>Option 3</h4></div>
-                <div><h4>Option 4</h4></div>
+                <div className="option">
+                    <p>Option 1</p>
+                </div>
+                <div className="option">
+                    <p>Option 2</p>
+                </div>
+                <div className="option">
+                    <p>Option 3</p>
+                </div>
+                <div className="option">
+                    <p>Option 4</p>
+                </div>
             </div>
         </section>
     )
