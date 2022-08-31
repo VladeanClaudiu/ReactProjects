01f60971446ce971bec403c202da7efcf2ec5dbc
VladeanClaudiu <claudiuver3.2@gmail.com>
2022-08-31 10:09:34 +0100

    created a new component called question

--- react-quiz-app/src/App.jsx
+++ react-quiz-app/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import StartPage from './components/StartPage'
+import Question from './components/Question'
 
 function App() {
   const [gameStart, setGameStart] = useState(false)
@@ -14,7 +15,9 @@ function App() {
       {!gameStart && <StartPage 
       handleClick={changeGameState}
       />}
-      {gameStart && <This />}
+      {gameStart && 
+      <section className='qeustion-section'><Question />
+      </section>}
       {/* ^^^ main quiz component to be added here :) */}
     </main>
   )
---
+++ react-quiz-app/src/components/Question.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Question() {
+    return(
+        <section className="question">
+             <h3 className="question-head">Question</h3>
+            <div className="question-options">
+                <div><h4>Option 1</h4></div>
+                <div><h4>Option 2</h4></div>
+                <div><h4>Option 3</h4></div>
+                <div><h4>Option 4</h4></div>
+            </div>
+        </section>
+    )
+}
